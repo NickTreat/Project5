0d5dcb2f062aa288052d97f872cdd3cefe95d985
Nick Treat <142675614+NickTreat@users.noreply.github.com>
2023-10-21 12:04:04 -0700

    undoing the chaos

--- java/main.js
+++ java/main.js
@@ -5,19 +5,3 @@ var ready = (callback) => {
 ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
-
-const body = document.querySelector('body');
-const modeToggle = document.getElementById('mode-toggle');
-const modeStatus = document.querySelector('.mode-status');
-
-function toggleMode() {
-  body.classList.toggle('dark-mode');
-
-  const modeMessage = body.classList.contains('dark-mode') ?
-    'Dark Mode' 
-    : "Light Mode"
-
-  modeStatus.innerText = "Currently in " + modeMessage;
-}
-
-modeToggle.addEventListener('click', toggleMode);
